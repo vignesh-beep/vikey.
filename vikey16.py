a6fcbe4400c471bbaa9f0dc1826b24b4622e25e2
vignesh-beep <52270714+vignesh-beep@users.noreply.github.com>
2019-07-01 11:28:22 +0530

    Create vikey16.py

---
+++ vikey16.py
@@ -0,0 +1,14 @@
+def print_factors(x):
+   
+
+   print("The factors of",x,"are:")
+   for i in range(1, x + 1):
+       if x % i == 0:
+           print(i)
+
+
+num1 = 320
+
+
+
+print_factors(num1)

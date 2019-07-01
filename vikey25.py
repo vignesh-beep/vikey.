ed6d6f246a3030df3d3742b11ff40c77045dbf9c
vignesh-beep <52270714+vignesh-beep@users.noreply.github.com>
2019-07-01 14:33:20 +0530

    Create vikey25.py

---
+++ vikey25.py
@@ -0,0 +1,5 @@
+number1 = input ("Enter number1")
+number1_two = input("Enter another number1")
+print("Printing type of input value")
+print ("type of number1 ", type(number1))
+print ("type of number1_two ", type(number1_two ))
